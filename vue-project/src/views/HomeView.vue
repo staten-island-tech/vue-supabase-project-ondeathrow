477e0fe551f34f695752d22888bd46974d8c0394
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-04-05 10:14:55 -0400

    deez nuts

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="login">
+    async function signUpNewUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+    options: {
+      emailRedirectTo: 'https://example.com/welcome',
+    },
+  })
+}
+
+<!--     <h2>Magic Link</h2>
+
+<p>come gamble</p>
+<p><a href="{{ .ConfirmationURL }}">Log In</a></p> -->
+  </div>
+</template>
+
+<script>
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style  scoped>
+
+</style>
