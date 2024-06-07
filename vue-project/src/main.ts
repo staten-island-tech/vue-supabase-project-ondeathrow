800619446cf50aa5ab19c0f154672af4c9fbfaca
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-06-06 21:21:17 -0400

    Update main.ts

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,1 +1,15 @@
+import './assets/main.css'
 
+import { createApp } from 'vue'
+import { createPinia, PiniaVuePlugin, defineStore } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const pinata = createPinia
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
