4751e26e41a379348999cd12e7be0ffad2331537
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-06-07 00:04:51 -0400

    Update HomeView.vue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,6 +3,18 @@ import { ref } from "vue";
 const email = ref("");
 const password = ref("");
 
+    <div class="signup">
+      <h1>sign over here</h1>
+      <form @submit.prevent="register()">
+        <label for="email">email</label>
+        <input type="email" v-model="email" id="email" />
+        <label for="password">password</label>
+        <input type="password" v-model="password" id="password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  </div>
+
 async function register() {
   try {
     console.log("aaa");
@@ -21,7 +33,7 @@ async function register() {
 
     <div class="signin">
       <h1>sign over here</h1>
-      <form @submit.prevent="register()">
+      <form @submit.prevent="login()">
         <label for="email">email</label>
         <input type="email" v-model="email" id="email" />
         <label for="password">password</label>
