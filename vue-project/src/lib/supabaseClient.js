aa577def2027374107ad22468779738fce94dc32
samanthayu27 <samanthay27@nycstudents.net>
2024-06-06 23:45:52 -0400

    Revert "Update and rename test.js to supabaseClient.js"
    This reverts commit 8e0faa2f1951d4a44beebb8991740d6cd8fd0168.

--- vue-project/src/lib/supabaseClient.js
+++
@@ -1,2 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient('https://fpvrosdsebauhpjtvndv.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdnJvc2RzZWJhdWhwanR2bmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTIxNjksImV4cCI6MjAyNzcyODE2OX0.NkD9tfJprT5ut2m06leZPn9lk26xtinZoCMKq_mDegQ>')
