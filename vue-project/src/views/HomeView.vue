fa8cfc2d296ed2f4a60fd563d4343c07475dae2f
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-06-11 20:11:03 -0400

    Update HomeView.vue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,4 +1,21 @@
-<template>
+<template> ‹div> ‹div class-"signup">
+<h1>sign up over here</h1> <form @submit.prevent "register")
+‹label for-"email"›email‹/label›
+<input type-"email" v-model "email" id-"email" /> ‹label for-"password"›password‹/label›
+<input type-"password" v-model-"password" id-"password" /> ‹button type-"submit"›Sign Up‹/button>
+</form> </div>
+‹div class-"signin"› <h1>sign in over here</h1> <form @submit.prevent-"login("› ‹label for="email"›email‹/label>
+<input type-"loginemail" v-model-"user" id-"loginemail" /> <label for-"password"›password‹/labels
+<input type- "loginpassword" v-model-"loginpass" id-"loginpassword" /> <button type-"submit"›Sign In‹/buttons
+</form> < div> </div> </template>
+<script setup>
+import { supabase } from "… ./11b/supabaseclient.js" import { ref } from "vue" const email - ref(*"); const password - ref("");
+const user - ref("");
+const loginpass - ref ("*) ;
+async function register) {
+console. log("hii").
+try {
+const { error } = await supabase.auth. signUp({
       <div class="signup">
         <h1>sign up over here</h1>
         <form @submit.prevent="register()">
